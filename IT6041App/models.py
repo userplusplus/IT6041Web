38d4ee0158da3f6a85dec3a80e6b9f375eabe0c1
Dominic Martindale <awalletsdream@gmail.com>
2020-10-30 17:20:42 +1300

    Created staff model
    added a django orm moddel for the mongodb for staff

--- IT6041App/models.py
+++ IT6041App/models.py
@@ -19,4 +19,19 @@ class Products(models.Model):
     def __str__(self):
         return self.product_name
 
+class Staff(models.Model):
+    staff_full_name = models.CharField(max_length=200)
+    work_email = models.EmailField(max_length=200)
+    work_phone = models.CharField(max_length=20)
+    mobile_phone = models.CharField(max_length=20)
+    department_role = models.CharField(max_length=200)
 
+    # Note that as in the WDD, Staff.department is effectively 3 different roles
+    # that can be represented by the choice of 3 strings. We will have
+    # to ensure filtered input to these strings however. 
+
+    class Meta:
+        ordering = ('-staff_name',)
+
+    def __str__(self):
+        return self.staff_name

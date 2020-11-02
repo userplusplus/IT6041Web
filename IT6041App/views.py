069e927f5414772ba730b692ca85908111d62f77
Dominic Martindale <awalletsdream@gmail.com>
2020-11-02 22:11:21 +1300

    staff page creation

--- IT6041App/views.py
+++ IT6041App/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'IT6041App/index.html', {'title': 'GreenWorlds EcoStore'})
 
+def staff(request):
+    return render(request, 'IT6041App/staff.html', {'title': 'GreenWorlds EcoStore'})
+

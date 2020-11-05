b5075a3843cb51a8947d87e608b293227cee3234
Charlie Maru <charliemaru@gmail.com>
2020-11-06 03:21:21 +1300

    working on Cart/Checkout
    creating cart/checkout pages and products on index pages to view.

--- IT6041App/urls.py
+++ IT6041App/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- IT6041App/views.py
+++ IT6041App/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'IT6041App/index.html', {'title': 'GreenWorlds EcoStore'})
+    context = {}
+    return render(request, 'IT6041App/index.html', context)
 
+
+def cart(request):
+    context = {}
+    return render(request, 'IT6041App/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'IT6041App/checkout.html', context)
